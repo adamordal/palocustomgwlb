d7e798205aef03bb97cf9c33bb4d23578372a25b
Adam Ordal <adam.ordal@anm.com>
2024-12-12 06:15:06 -0700

    updates to cleanup spacing

--- existing-net/routes.tf
+++ existing-net/routes.tf
@@ -51,13 +51,6 @@ resource "aws_route" "sec_txgw_rt_route_1" {
   vpc_endpoint_id        = aws_vpc_endpoint.gwlbe_ns_endpoint[each.key % length(aws_vpc_endpoint.gwlbe_ns_endpoint)].id
 }
 
-#resource "aws_route" "nat_rt_route_0" {
-#  for_each               = { for idx, rt_id in local.nat_rts : idx => rt_id }
-#  route_table_id         = each.value
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id             = data.aws_internet_gateway.sec_vpc_igw.id
-#}
-
 resource "aws_route" "nat_rt_route_1" {
   for_each               = { for idx, rt_id in local.nat_rts : idx => rt_id }
   route_table_id         = each.value
