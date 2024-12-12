8a5446a10a6ad840670336f95fc5bf972a6457f5
Adam Ordal <adam.ordal@anm.com>
2024-12-12 06:09:47 -0700

    cleanup spacing & remove commented out code

--- existing-net/transit_gateway.tf
+++ existing-net/transit_gateway.tf
@@ -6,15 +6,3 @@ data "aws_ec2_transit_gateway" "transit_gw" {
   id = var.transit_gw_id
 }
 
-#resource "aws_ec2_transit_gateway_vpc_attachment" "sec_txgw_attach" {
-#  subnet_ids             = local.sec_txgw_subnets
-#  transit_gateway_id     = data.aws_ec2_transit_gateway.transit_gw.id
-#  vpc_id                 = data.aws_vpc.sec_vpc.id
-#  appliance_mode_support = "enable"
-#
-#  tags = {
-#    Name = "sec-txgw-attach-${random_id.deployment_id.hex}"
-#  }
-#
-#  depends_on = []
-#}
